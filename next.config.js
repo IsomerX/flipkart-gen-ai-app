bba96752533cbf802ab17077ef5140164539aba7
IsomerX <49413460+IsomerX@users.noreply.github.com>
2023-08-20 17:50:30 +0530

    next conf

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  images: {
-    domains: ["links.papareact.com"],
-  },
-  experimental: {
-    appDir: true,
-  },
+    images: {
+        domains: ["links.papareact.com"],
+    },
+    experimental: {
+        appDir: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
